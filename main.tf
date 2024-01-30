bf4632ab5b2c9c55ca57f957617bcf36dfba3e2e
havingchan <61160874@qq.com>
2024-01-30 08:01:22 +0000

    update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
 ## PUT TERRAFORM CLOUD BLOCK HERE!  ##
 
 terraform {
-  
-  
-  
+  cloud {
+    organization = "having-training"
+
+    workspaces {
+      name = "tf-cloud-test"
+    }
+  }
+
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -17,14 +23,17 @@ terraform {
 
 
 # Variable blocks directly within the main.tf. No arguments necessary.
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+#variable "aws_access_key" {}
+#variable "aws_secret_key" {}
+variable "region" {
+  sensitive = true
+}
 
 # provider arguments call on the variables which then call on terraform.tfvars for the values.
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = "us-east-2"
+  #access_key = var.aws_access_key
+  #secret_key = var.aws_secret_key
+  region = var.region
 }
 
 # Add .gitignore file in this directory with the terraform.tfvars
